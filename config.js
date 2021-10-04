ff0b7917e3ecd51110ae9b6f35340b54734dcf0b
KonyTech <nicolas.cortella@protonmail.com>
2021-10-04 23:20:21 +0200

    Added an example network in the general config file

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ const hardhatConfig = {
     defaultNetwork: "rinkeby",
     networks: {
       hardhat: { // Default network, can be removed from here and will still work
-      }
+      },
+      //
+      //networkName: {
+      //  url: "https://mainnet.infura.io/TOKEN",
+      //  accounts: {
+      //    mnemonic: "ENTER YOUR MNEMONIC HERE"
+      //  }
+      //}
     },
     solidity: "0.8.4",
   }
