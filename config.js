ceaf0920314fe96e7517ccef78fff6a8f04536f4
KonyTech <nicolas.cortella@protonmail.com>
2021-10-04 23:18:28 +0200

    Moved hardhat config upward

---
+++ config.js
@@ -0,0 +1,12 @@
+const hardhatConfig = {
+    defaultNetwork: "rinkeby",
+    networks: {
+      hardhat: { // Default network, can be removed from here and will still work
+      }
+    },
+    solidity: "0.8.4",
+  }
+
+module.exports = {
+    hardhatConfig
+};
--- src/eth_contracts/hardhat.config.js
+++ src/eth_contracts/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+const { hardhatConfig } = require("../../config");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -16,17 +17,4 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: { // Default network, can be removed from here and will still work
-    },
-    rinkeby: {
-      url: "https://rinkeby.infura.io/v3/a411c7e6b1c6470f8bbc2399c51648df",
-      accounts: {
-        mnemonic: "cushion soft soap strong trigger fluid rally toast tornado web salute ginger"
-      }
-    }
-  },
-  solidity: "0.8.4",
-};
+module.exports = hardhatConfig;
